265df3742ab042ca490a07d9c3d9d205421fa86e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-08-02 13:34:38 +0200

    Create QuestionView

---
+++ FlashMath2/QuestionView.swift
@@ -0,0 +1,42 @@
+//
+//  QuestionView.swift
+//  FlashMath2
+//
+//  Created by Eric on 02/08/2024.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var question = HowManyMultiplesQuestion()
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+            Text("Question 1")
+                .fontDesign(.rounded)
+                .fontWeight(.black)
+
+            Text(question.questionText)
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+
+            ForEach(0..<question.allAnswers.count, id: \.self) { index in
+                Button {
+
+                } label: {
+                    Text(question.allAnswers[index].formatted())
+                }
+            }
+
+            Spacer()
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
