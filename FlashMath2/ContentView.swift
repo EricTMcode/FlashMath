22d6064d6b9b87042d877d25f32755112372e3ed
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-08-02 13:14:25 +0200

    Initial Commit

---
+++ FlashMath2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashMath2
+//
+//  Created by Eric on 02/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashMath2/FlashMath2App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMath2App.swift
+//  FlashMath2
+//
+//  Created by Eric on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashMath2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
