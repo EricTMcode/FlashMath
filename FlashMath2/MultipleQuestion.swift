05da5c5367cf9f9cddd8590885a6350c96c9987f
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-08-06 21:22:44 +0200

    Create MultipleQuestion

---
+++ FlashMath2/MultipleQuestion.swift
@@ -0,0 +1,35 @@
+//
+//  MultipleQuestion.swift
+//  FlashMath2
+//
+//  Created by Eric on 06/08/2024.
+//
+
+import Foundation
+
+struct MultipleQuestion {
+    var questionText: String
+    var correctAnswer: Int
+    var allAnswers: [Int]
+
+    init() {
+        let allMultiples = (2...12).shuffled()
+        let multiple = Int.random(in: 2...12)
+
+        correctAnswer = Int.random(in: 1...12) * multiple
+        questionText = "Multiple of \(multiple)?"
+
+        allAnswers = (0...3).map { index in
+            if multiple == 2 {
+                allMultiples[index] * multiple + 1
+            } else {
+                allMultiples[index] * multiple + 2
+            }
+        }
+
+        allAnswers.append(correctAnswer)
+        allAnswers.shuffle()
+    }
+
+
+}
