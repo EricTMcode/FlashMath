27f760b14594348b41ed8ce7da0e96e574ab06d3
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-08-09 07:26:48 +0200

    Add styles for menuView and GameOverView

--- FlashMath2/Style/Styles.swift
+++ FlashMath2/Style/Styles.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 extension View {
+    func titleStyle() -> some View {
+        self
+            .font(.system(size: 48))
+            .fontWidth(.condensed)
+            .fontWeight(.black)
+            .multilineTextAlignment(.center)
+    }
+
+    func subtitleStyle() -> some View {
+        self
+            .font(.title.bold())
+            .padding(.bottom, 40)
+            .foregroundStyle(.secondary)
+    }
+
     func backgroundGradient() -> some View {
         self
             .background(LinearGradient(colors: [.indigo, .black], startPoint: .top, endPoint: .bottom))
@@ -33,5 +48,23 @@ struct QuestionButton: ButtonStyle {
 extension ButtonStyle where Self == QuestionButton {
     static func question(color: Color) -> QuestionButton {
         QuestionButton(color: color)
+    }
+}
+
+struct PrimaryButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title.bold())
+            .foregroundStyle(configuration.isPressed ? .white : .blue)
+            .padding(10)
+            .padding(.horizontal, 10)
+            .background(configuration.isPressed ? .blue : .white)
+            .clipShape(.capsule)
+    }
+}
+
+extension ButtonStyle where Self == PrimaryButton {
+    static var primary: PrimaryButton {
+        PrimaryButton()
     }
 }
