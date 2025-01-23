4f829a0560e4f7098d7fe9fe706380ced1fb0780
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-23 14:53:08 +0100

    Add time pressure

--- FlashMath2/QuestionView.swift
+++ FlashMath2/QuestionView.swift
@@ -12,6 +12,14 @@ struct QuestionView: View {
 
     let colors: [Color] = [.purple, .blue, .green, .pink, .orange].shuffled()
 
+    let timer = Timer.publish(every: 1 / 30, on: .main, in: .common).autoconnect()
+
+    @State private var timeUsed = 0.0
+
+    var timeRemaining: Double {
+        max(0, viewModel.timeAllowed - timeUsed)
+    }
+
     var body: some View {
         VStack {
             Spacer()
@@ -35,12 +43,25 @@ struct QuestionView: View {
 
             Spacer()
             Spacer()
+
+            Text("Time: " + timeRemaining.formatted(.number.precision(.fractionLength(2))))
+                .font(.largeTitle)
+                .monospacedDigit()
         }
         .padding(.horizontal)
+        .onReceive(timer) { time in
+            timeUsed += 1 / 30
+
+            if timeUsed >= viewModel.timeAllowed {
+                // game over
+            }
+        }
         .transition(.push(from: .trailing))
     }
 
     func select(_ number: Int) {
+        timer.upstream.connect().cancel()
+
         withAnimation {
             viewModel.check(answer: number)
         }
