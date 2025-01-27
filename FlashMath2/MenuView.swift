040cf1f237f21457425e41820e5b911f3fa38751
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-27 14:53:34 +0100

    Add logo and phaseAnimator

--- FlashMath2/MenuView.swift
+++ FlashMath2/MenuView.swift
@@ -15,6 +15,16 @@ struct MenuView: View {
         VStack {
             Spacer()
 
+            Image(.logo)
+                .clipShape(.rect(cornerRadius: 20))
+                .padding(.bottom, 40)
+                .phaseAnimator([false, true]) { content, phase in
+                    content
+                        .shadow(color: .purple, radius: phase ? 20 : 40)
+                } animation: { _ in
+                        .easeIn(duration: 1)
+                }
+
             Text("FLASH MATH")
                 .titleStyle()
             Text("Last Score: \(lastScore)")
