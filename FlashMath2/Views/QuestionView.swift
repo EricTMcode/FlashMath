35debfbd028b561f6e2f3ea86bbe80fb08c78697
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-06-26 16:14:36 +0200

    Add blendMode

--- FlashMath2/Views/QuestionView.swift
+++ FlashMath2/Views/QuestionView.swift
@@ -9,29 +9,29 @@ import SwiftUI
 
 struct QuestionView: View {
     @Environment(ViewModel.self) var viewModel
-    
+
     let colors: [Color] = [.purple, .blue, .green, .pink, .orange].shuffled()
-    
+
     let timer = Timer.publish(every: 1 / 30, on: .main, in: .common).autoconnect()
-    
+
     @State private var timeUsed = 0.0
-    
+
     var timeRemaining: Double {
         max(0, viewModel.timeAllowed - timeUsed)
     }
-    
+
     var body: some View {
         VStack {
             Spacer()
-            
+
             Text("Question \(viewModel.questionNumber)")
                 .fontDesign(.rounded)
                 .fontWeight(.black)
-            
+
             Text(viewModel.questionText)
                 .multilineTextAlignment(.center)
                 .font(.largeTitle)
-            
+
             ForEach(0..<viewModel.allAnswers.count, id: \.self) { i in
                 Button {
                     select(viewModel.allAnswers[i])
@@ -40,28 +40,38 @@ struct QuestionView: View {
                 }
                 .buttonStyle(.question(color: colors[i]))
             }
-            
+
             Spacer()
             Spacer()
-            
-            Text("Time: " + timeRemaining.formatted(.number.precision(.fractionLength(2))))
-                .font(.largeTitle)
-                .monospacedDigit()
+
+            ZStack {
+                Capsule()
+                    .fill(.white.gradient)
+                    .frame(height: 50)
+                    .containerRelativeFrame(.horizontal) { value, axis in
+                        value * timeRemaining / viewModel.timeAllowed
+                    }
+
+                Text("Time: " + timeRemaining.formatted(.number.precision(.fractionLength(2))))
+                    .font(.largeTitle)
+                    .monospacedDigit()
+                    .blendMode(.difference)
+            }
         }
         .padding(.horizontal)
         .onReceive(timer) { time in
             timeUsed += 1 / 30
-            
+
             if timeUsed >= viewModel.timeAllowed {
                 // game over
             }
         }
         .transition(.push(from: .trailing))
     }
-    
+
     func select(_ number: Int) {
         timer.upstream.connect().cancel()
-        
+
         withAnimation {
             viewModel.check(answer: number)
         }
