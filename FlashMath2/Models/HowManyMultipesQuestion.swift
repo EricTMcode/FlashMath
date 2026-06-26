91e4f3cdcda6d2f24d2c877d85deecce8308dc74
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-06-26 14:53:14 +0200

    Create HowManyMultipleQuestion struct

---
+++ FlashMath2/Models/HowManyMultipesQuestion.swift
@@ -0,0 +1,28 @@
+//
+//  HowManyMultipesQuestion.swift
+//  FlashMath2
+//
+//  Created by Eric on 26/06/2026.
+//
+
+import Foundation
+
+struct HowManyMultipesQuestion {
+    var questionText: String
+    var correctAnswer: Int
+    var allAnswers: [Int]
+
+    init() {
+        var numbers = (2...12).shuffled()
+        correctAnswer = numbers.removeFirst()
+
+        let multiple = Int.random(in: 2...12)
+        let adjustement = Int.random(in: 0...multiple - 1)
+        let targetNumber = correctAnswer * multiple + adjustement
+
+        questionText = "How many \(multiple)s in \(targetNumber)?"
+        allAnswers = Array(numbers.prefix(4))
+        allAnswers.append(correctAnswer)
+        allAnswers.shuffle()
+    }
+}
